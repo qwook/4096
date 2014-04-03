cef8d7c2bfa811151c94565db89961c2da7fe94f
Henry <henryquoctran@gmail.com>
2014-04-03 14:10:40 -0700

    Experimental swiping code

--- src/index.js
+++ src/index.js
@@ -38,6 +38,68 @@ document.onkeydown = function(e)
     }
 }
 
+// credits to original 2048. this is something that I probably could have figured out but didn't have the time to.
+game.dom.addEventListener(this.eventTouchstart, function (event)
+{
+
+    if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
+        event.targetTouches > 1) {
+        return; // Ignore if touching with more than 1 finger
+    }
+
+    if (window.navigator.msPointerEnabled)
+    {
+        touchStartClientX = event.pageX;
+        touchStartClientY = event.pageY;
+    }
+    else
+    {
+        touchStartClientX = event.touches[0].clientX;
+        touchStartClientY = event.touches[0].clientY;
+    }
+
+    event.preventDefault();
+
+});
+
+game.dom.addEventListener(this.eventTouchmove, function (event)
+{
+    event.preventDefault();
+});
+
+game.dom.addEventListener(this.eventTouchend, function (event) {
+    if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
+        event.targetTouches > 0)
+    {
+        return; // Ignore if still touching with one or more fingers
+    }
+
+    var touchEndClientX, touchEndClientY;
+
+    if (window.navigator.msPointerEnabled)
+    {
+        touchEndClientX = event.pageX;
+        touchEndClientY = event.pageY;
+    }
+    else
+    {
+        touchEndClientX = event.changedTouches[0].clientX;
+        touchEndClientY = event.changedTouches[0].clientY;
+    }
+
+    var dx = touchEndClientX - touchStartClientX;
+    var absDx = Math.abs(dx);
+
+    var dy = touchEndClientY - touchStartClientY;
+    var absDy = Math.abs(dy);
+
+    if (Math.max(absDx, absDy) > 10)
+    {
+        // (right : left) : (down : up)
+        absDx > absDy ? (dx > 0 ? game.moveRight() : game.moveLeft()) : (dy > 0 ? game.moveDown() : game.moveUp())
+    }
+});
+
 game.dom.getElementsByClassName("retry")[0].onclick = function(e)
 {
     game.reset();
